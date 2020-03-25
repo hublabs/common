652f0cf8e4aae2efbe716a1da7317ecb75d2661a
Jang Jaehue <jaehue@jang.io>
2020-03-25 10:52:16 +0800

    #9 Add new error template(ErrorParameterParsingFailed, ErrorInvalidStatus)

--- api/errors.go
+++ api/errors.go
@@ -10,25 +10,29 @@ type ErrorTemplate Error
 
 var (
 	// System Error
-	ErrorUnknown            = ErrorTemplate{Code: 10001, Message: "Unknown error"}
-	ErrorServiceUnavailable = ErrorTemplate{Code: 10002, Message: "Service unavailable"}
-	ErrorRemoteService      = ErrorTemplate{Code: 10003, Message: "Remote service error"}
-	ErrorRateLimit          = ErrorTemplate{Code: 10004, Message: "Rate limit"}
-	ErrorPermissionDenied   = ErrorTemplate{Code: 10005, Message: "Permission denied", status: http.StatusForbidden}
-	ErrorIllegalRequest     = ErrorTemplate{Code: 10006, Message: "Illegal request", status: http.StatusBadRequest}
-	ErrorHTTPMethod         = ErrorTemplate{Code: 10007, Message: "HTTP method is not suported for this request", status: http.StatusMethodNotAllowed}
-	ErrorParameter          = ErrorTemplate{Code: 10008, Message: "Parameter error", status: http.StatusBadRequest}
-	ErrorMissParameter      = ErrorTemplate{Code: 10009, Message: "Miss required parameter", status: http.StatusBadRequest}
-	ErrorDB                 = ErrorTemplate{Code: 10010, Message: "DB error"}
-	ErrorTokenInvaild       = ErrorTemplate{Code: 10011, Message: "Token invaild", status: http.StatusUnauthorized}
-	ErrorMissToken          = ErrorTemplate{Code: 10012, Message: "Miss token", status: http.StatusUnauthorized}
-	ErrorVersion            = ErrorTemplate{Code: 10013, Message: "API version %s invalid"}
-	ErrorNotFound           = ErrorTemplate{Code: 10014, Message: "Resource not found", status: http.StatusNotFound}
-	ErrorInvalidFields      = ErrorTemplate{Code: 10015, Message: "Invalid fields [ %v ]", status: http.StatusBadRequest}
+	ErrorUnknown                = ErrorTemplate{Code: 10001, Message: "Unknown error"}
+	ErrorServiceUnavailable     = ErrorTemplate{Code: 10002, Message: "Service unavailable"}
+	ErrorRemoteService          = ErrorTemplate{Code: 10003, Message: "Remote service error"}
+	ErrorRateLimit              = ErrorTemplate{Code: 10004, Message: "Rate limit"}
+	ErrorPermissionDenied       = ErrorTemplate{Code: 10005, Message: "Permission denied", status: http.StatusForbidden}
+	ErrorIllegalRequest         = ErrorTemplate{Code: 10006, Message: "Illegal request", status: http.StatusBadRequest}
+	ErrorHTTPMethod             = ErrorTemplate{Code: 10007, Message: "HTTP method is not suported for this request", status: http.StatusMethodNotAllowed}
+	ErrorParameter              = ErrorTemplate{Code: 10008, Message: "Parameter error", status: http.StatusBadRequest}
+	ErrorMissParameter          = ErrorTemplate{Code: 10009, Message: "Miss required parameter", status: http.StatusBadRequest}
+	ErrorDB                     = ErrorTemplate{Code: 10010, Message: "DB error"}
+	ErrorTokenInvaild           = ErrorTemplate{Code: 10011, Message: "Token invaild", status: http.StatusUnauthorized}
+	ErrorMissToken              = ErrorTemplate{Code: 10012, Message: "Miss token", status: http.StatusUnauthorized}
+	ErrorVersion                = ErrorTemplate{Code: 10013, Message: "API version %s invalid"}
+	ErrorNotFound               = ErrorTemplate{Code: 10014, Message: "Resource not found", status: http.StatusNotFound}
+	ErrorInvalidFields          = ErrorTemplate{Code: 10015, Message: "Invalid fields [ %v ]", status: http.StatusBadRequest}
+	ErrorParameterParsingFailed = ErrorTemplate{Code: 10016, Message: "Fail to parse parameter [ %v ]", status: http.StatusBadRequest}
 
 	// Product Error
 	ErrorInvalidCodeError = ErrorTemplate{Code: 20001, Message: "Code is invalid", status: http.StatusOK}
 	ErrorOutOfStockError  = ErrorTemplate{Code: 20002, Message: "Out of stock", status: http.StatusOK}
+
+	// Order Error
+	ErrorInvalidStatus = ErrorTemplate{Code: 3001, Message: "Invalid status", status: http.StatusForbidden}
 )
 
 var errorMessagePrefix = "unknown service"
